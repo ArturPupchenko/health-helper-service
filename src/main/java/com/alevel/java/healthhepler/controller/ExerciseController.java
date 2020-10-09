4daf1aec4894e4fd89c41e6d609521fb1e023947
arturpupchenko <PupchenkoArtur@gmail.com>
2020-10-09 19:09:49 +0300

    Edite static resources.

--- src/main/java/com/alevel/java/healthhepler/controller/ExerciseController.java
+++ src/main/java/com/alevel/java/healthhepler/controller/ExerciseController.java
@@ -1,19 +1,14 @@
 package com.alevel.java.healthhepler.controller;
 
 import com.alevel.java.healthhepler.model.exercise.response.ExerciseResponse;
-import com.alevel.java.healthhepler.model.training.request.SaveTrainingRequest;
-import com.alevel.java.healthhepler.model.training.response.TrainingResponse;
-import com.alevel.java.healthhepler.model.user.response.UserResponse;
 import com.alevel.java.healthhepler.service.exercise.ExerciseOperations;
-import com.alevel.java.healthhepler.service.training.TrainingOperations;
 import io.swagger.v3.oas.annotations.Parameter;
 import org.springdoc.core.converters.models.PageableAsQueryParam;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/exercises")
