fed0e1d708fde32fd770ed3ef625b85786358741
arturpupchenko <PupchenkoArtur@gmail.com>
2020-10-13 19:50:41 +0300

    Edited migration V2_populate.sql

--- src/main/resources/db/migration/V2__populate.sql
+++ src/main/resources/db/migration/V2__populate.sql
@@ -1,7 +1,18 @@
-insert into exercises (name)
-values ('Barbell bench press');
-insert into exercises (name)
-values ('Barbell squats');
-insert into exercises (name)
-values ('Barbell deadlifts');
+insert into exercises (name, description)
+values ('Barbell bench press', 'Position yourself with your back down on a flat bench placing the hands up on the bar using an underhand grip slightly wider than shoulder width apart.
+    After taking a deep breath in, extend the elbows and lift the bar up off the rack until elbows are extended but not locked. Pause for a brief movement as you fully extend your breath.
+    Lower the weight directly down to the chest as you breath inwards, keeping the movement pattern slow and steady.
+    Once just above the chest, pause, and then press directly up again, exhaling your breath.
+    Continue until all reps are completed and then re-rack the barbell.');
+insert into exercises (name, description)
+values ('Barbell deadlifts','Place a barbell in a clear space on the floor. (Make sure there are no obstructions in your way.
+    Stand facing the barbell with your legs about 4-6 inches from the bar. Place your feet shoulder width apart.Your feet can be pointed straight ahead or turned outwards slightly.
+    Squat down, keeping your back straight and grip the bar with an overhand grip at shoulder width.
+    Keep your arms fully extended and stand up with the barbell. As you lift the barbell, your hips and shoulders should rise together and your back should be straight.
+    As you reach the top of the lift and are standing straight, rotate your shoulders back slightly until you feel a slight stretch in them.
+    Lower the barbell back to the floor in the same squatting motion you used to lift it.');
+insert into exercises (name,description)
+values ('Barbell squats', 'Begin by positioning a barbell at or just below shoulder level in the squat rack. Behind the squat rack, place a tall wooden box or chair. Place yourself underneath the barbell. Make sure the bar is across your upper traps and NOT on your neck.
+    Place your feet outside of shoulder-width with your toes pointed slightly out. Brace your core and keep your chest up.
+    Bend at the knees as you drive your hips back. Lower yourself slowly until your butt touches the box. Pause then drive your hips forward to return to the starting position.');
 
